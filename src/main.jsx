63406047363d127ee693f8edcaa327bf54957ec1
Matheus Pedro <121190209+MatheusDevb@users.noreply.github.com>
2025-11-12 16:17:29 -0300

    Set up project structure and install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  // Aqui adicionaremos as outras páginas (Sobre, Contato, etc.) no futuro
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Bem-vindo ao site da Devéra!</p>
+    </div>
+  );
+}
+
+export default Home;
