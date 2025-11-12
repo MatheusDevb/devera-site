83db113f48f868be8229450ce740d0d99ff39d6e
Matheus Pedro <121190209+MatheusDevb@users.noreply.github.com>
2025-11-12 16:46:28 -0300

    Style Navbar using CSS Modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,38 @@
+// src/components/Navbar.jsx
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css"; // 1. Importa o CSS Module
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Devéra</Link>
-      <ul>
+    // 2. Usa a classe .navbar no container principal
+    <nav className={styles.navbar}>
+      {/* 3. Usa a classe .logo para o link da home */}
+      <Link to="/" className={styles.logo}>
+        Devéra
+      </Link>
+
+      {/* 4. Usa a classe .navList para a lista */}
+      <ul className={styles.navList}>
         <li>
-          <Link to="/sobre">Sobre</Link>
+          {/* 5. E a classe .navLink para cada link */}
+          <Link to="/sobre" className={styles.navLink}>
+            Sobre
+          </Link>
         </li>
         <li>
-          <Link to="/servicos">Serviços</Link>
+          <Link to="/servicos" className={styles.navLink}>
+            Serviços
+          </Link>
         </li>
         <li>
-          <Link to="/portfolio">Portfólio</Link>
+          <Link to="/portfolio" className={styles.navLink}>
+            Portfólio
+          </Link>
         </li>
         <li>
-          <Link to="/contato">Contato</Link>
+          <Link to="/contato" className={styles.navLink}>
+            Contato
+          </Link>
         </li>
       </ul>
     </nav>
