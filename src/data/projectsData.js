3f02381fcfadf926e1acf52108a27ea5ad658bcf
Matheus Pedro <121190209+MatheusDevb@users.noreply.github.com>
2026-01-14 18:57:30 -0300

    update fotos portfolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,6 +4,11 @@
 import project1Image from "../assets/projeto-1.png";
 import project2Image from "../assets/projeto-2.png";
 import project3Image from "../assets/projeto-dataage-1.png";
+import project4Image from "../assets/clinica1.jpg";
+import project5Image from "../assets/clinica2.jpg";
+import project6Image from "../assets/auto-sport1.jpg";
+import project7Image from "../assets/auto-sport2.jpg";
+import project8Image from "../assets/auto-sport3.jpg";
 
 export const projects = [
   {
@@ -19,7 +24,7 @@ export const projects = [
       "A clínica precisava de uma presença online profissional para atrair novos pacientes e facilitar o agendamento de consultas. O site antigo era lento e não funcionava bem em dispositivos móveis.",
     solution:
       "Desenvolvemos um site institucional moderno e rápido, com foco na experiência do usuário. A nova interface é intuitiva, e a implementação de um design responsivo garante uma experiência perfeita em qualquer tela. Um formulário de pré-agendamento foi integrado para otimizar o fluxo de novos pacientes.",
-    gallery: [project1Image, project2Image, project3Image], // Adicionar mais imagens do projeto
+    gallery: [project1Image, project4Image, project5Image], // Adicionar mais imagens do projeto
   },
   {
     id: "projeto-2",
@@ -34,7 +39,7 @@ export const projects = [
       "O desafio era criar um portfólio online para mostrar a qualidade do trabalho artesanal da tapeçaria, que até então dependia apenas de fotos em redes sociais.",
     solution:
       "Criamos uma galeria de alta resolução com filtros por tipo de serviço. O design foca na qualidade visual dos projetos, transmitindo confiança e profissionalismo. Otimizações de SEO foram implementadas para atrair clientes locais que buscam por serviços de tapeçaria automotiva.",
-    gallery: [project2Image, project1Image],
+    gallery: [project2Image, project6Image, project7Image, project8Image],
   },
   {
     id: "projeto-3",
